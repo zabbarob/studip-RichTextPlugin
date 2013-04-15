248c2b7e814c591a737da99a4e786f3fb28d56b8
Robert Costa <zabbarob@gmail.com>
2013-04-15 16:11:14 +0200

    added infobox

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,8 @@
 (function ($) {
     $(function() {
-        var editor = new wysihtml5.Editor("textarea", {
-            toolbar:      "toolbar",
+        var editor = new wysihtml5.Editor("wysihtml5-textarea", {
+            toolbar:      "wysihtml5-toolbar",
             parserRules:  wysihtml5ParserRules
         });
-
-        var log = document.getElementById("log");
-
-        editor
-            .on("load", function() {
-                log.innerHTML += "<div>load</div>";
-            })
-            .on("focus", function() {
-                log.innerHTML += "<div>focus</div>";
-            })
-            .on("blur", function() {
-                log.innerHTML += "<div>blur</div>";
-            })
-            .on("change", function() {
-                log.innerHTML += "<div>change</div>";
-            })
-            .on("paste", function() {
-                log.innerHTML += "<div>paste</div>";
-            })
-            .on("newword:composer", function() {
-                log.innerHTML += "<div>newword:composer</div>";
-            })
-            .on("undo:composer", function() {
-                log.innerHTML += "<div>undo:composer</div>";
-            })
-            .on("redo:composer", function() {
-                log.innerHTML += "<div>redo:composer</div>";
-            });
     });
 }(jQuery));
--- templates/show.php
+++ templates/show.php
@@ -1,22 +1,62 @@
-<h1>wysihtml5 - Simple Editor Example</h1>
+<?
+/**
+ * show.php - Template for displaying the RichText plugin.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * @author      Robert Costa <rcosta@uos.de>
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+ * @category    Stud.IP
+ */
 
-<p>
-  Uses a custom rule set that allows the following elements: <em>strong, b, em, i, a, span</em><br>
-  Links will automatically receive <i>target="_blank"</i> and <i>rel="nofollow"</i>. Check the source code of this page.
-</p>
+// show infobox
+$infobox_content[] = array(
+    'kategorie' => _('&Uuml;ber den Rich Text Editor:'),
+    'eintrag'   => array(
+        array(
+            'icon' => 'icons/16/black/info.png',
+            'text' => _('Teilnehmer k&ouml;nnen den RichText Editor verwenden, um gemeinsam Informationen zum Kurs zu editieren.')),
+        array(
+            'icon' => 'icons/16/black/info.png',
+            'text' => _('Folgende HTML Elemente sind erlaubt: <i>strong, b, em, i, a, span</i>.')),
+        array(
+            'icon' => 'icons/16/black/info.png',
+            'text' => _('Links erhalten automatisch die Attribute <i>target="_blank"</i> und <i>rel="nofollow"</i>.')),
+        array(
+            'text' => _('<small>powered by <a href="https://github.com/xing/wysihtml5" target="_blank">wysihtml5</a></small>'))));
 
+$infobox = array(
+    'picture' => 'infobox/board1.jpg',
+    'content' => $infobox_content);
+
+// show the toolbar and the actual editor component
+?>
 <form>
-  <div id="toolbar" style="display: none;">
+  <div id="wysihtml5-toolbar" style="display: none;">
     <a data-wysihtml5-command="bold" title="CTRL+B">bold</a> |
     <a data-wysihtml5-command="italic" title="CTRL+I">italic</a>
+    ||
+
+    <!-- Some wysihtml5 commands require extra parameters -->
+    <a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="red">red</a> |
+    <a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="green">green</a> |
+    <a data-wysihtml5-command="foreColor" data-wysihtml5-command-value="blue">blue</a>
+    ||
+
+    <!-- Some wysihtml5 commands like 'createLink' require extra paramaters specified by the user (eg. href) -->
+    <a data-wysihtml5-command="createLink">insert link</a>
+    <div data-wysihtml5-dialog="createLink" style="display: none;">
+      <label>Link: <input data-wysihtml5-dialog-field="href" value="http://" class="text"></label>
+      <a data-wysihtml5-dialog-action="save">OK</a>
+      <a data-wysihtml5-dialog-action="cancel">Cancel</a>
+    </div>
+    ||
+
     <a data-wysihtml5-action="change_view">switch to html view</a>
   </div>
-  <textarea id="textarea" placeholder="Enter text ..."></textarea>
-  <br><input type="reset" value="Reset form!">
+  <textarea id="wysihtml5-textarea" placeholder="Enter text ..."></textarea>
 </form>
-
-<h2>Events:</h2>
-<div id="log"></div>
-
-<small>powered by <a href="https://github.com/xing/wysihtml5" target="_blank">wysihtml5</a></small>
 
