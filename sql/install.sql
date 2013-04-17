c4cbf24fc6e3d89ecc3c576f7127ea774c218870
Robert Costa <zabbarob@gmail.com>
2013-04-17 16:06:49 +0200

    added CREATE/DROP TABLE to install.sql/uninstall.sql

--- sql/install.sql
+++ sql/install.sql
@@ -1,1 +1,17 @@
--- SQL
+-- install.sql - Creates tables for the RichText plugin.
+-- 
+-- This program is free software, you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License as
+-- published by the Free Software Foundation, either version 2 of
+-- the License, or (at your option) any later version.
+-- 
+-- @author      Robert Costa <zabbarob@gmail.com>
+-- @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+-- @category    Stud.IP
+--
+CREATE TABLE IF NOT EXISTS `plugin_rich_text` (
+  `range_id` varchar(32) COLLATE latin1_german1_ci NOT NULL DEFAULT '',
+  `body` text COLLATE latin1_german1_ci,
+  PRIMARY KEY (`range_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
+
--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -1,1 +1,17 @@
--- SQL
+-- uninstall.sql - Creates tables for the RichText plugin.
+-- 
+-- This program is free software, you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License as
+-- published by the Free Software Foundation, either version 2 of
+-- the License, or (at your option) any later version.
+-- 
+-- @author      Robert Costa <zabbarob@gmail.com>
+-- @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+-- @category    Stud.IP
+--
+-- NOTE user privileges are not automatically dropped
+-- see section 13.7.1.3, “GRANT Syntax” of MySQL 5.5 Reference Manual
+-- http://dev.mysql.com/doc/refman/5.5/en/grant.html
+--
+DROP TABLE IF EXISTS `plugin_rich_text`;
+
