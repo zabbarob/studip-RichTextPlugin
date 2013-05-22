d0cf974a4cd6da69a3df802f9660b9a89c2ab4d7
Robert Costa <zabbarob@gmail.com>
2013-05-22 15:19:12 +0200

    updated info box text, added link to HTML Purifier

---
+++ templates/infobox.php
@@ -0,0 +1,20 @@
+<?
+$infobox_content[] = array(
+    'kategorie' => _('&Uuml;ber den Rich Text Editor:'),
+    'eintrag'   => array(
+        array(
+            'icon' => 'icons/16/black/info.png',
+            'text' => _('Teilnehmer k&ouml;nnen den RichText Editor verwenden, um gemeinsam Informationen zum Kurs zu editieren.')),
+        array(
+            'icon' => 'icons/16/black/info.png',
+            'text' => _('Links erhalten automatisch die Attribute <i>target="_blank"</i> und <i>rel="nofollow"</i>.')),
+        array(
+            'icon' => 'icons/16/black/info.png',
+            'text' => _('Dateien k&ouml;nnen per Drag\'n\'Drop hochgeladen werden')),
+        array(
+            'text' => _('<small>powered by <a href="https://github.com/xing/wysihtml5" target="_blank">wysihtml5</a> and <a href="http://htmlpurifier.org/" target="_blank">HTML Purifier</a></small>'))));
+
+$infobox = array(
+    'picture' => 'infobox/board1.jpg',
+    'content' => $infobox_content);
+
--- templates/show.php
+++ templates/show.php
@@ -13,24 +13,7 @@
  */
 
 // show infobox
-$infobox_content[] = array(
-    'kategorie' => _('&Uuml;ber den Rich Text Editor:'),
-    'eintrag'   => array(
-        array(
-            'icon' => 'icons/16/black/info.png',
-            'text' => _('Teilnehmer k&ouml;nnen den RichText Editor verwenden, um gemeinsam Informationen zum Kurs zu editieren.')),
-        array(
-            'icon' => 'icons/16/black/info.png',
-            'text' => _('Folgende HTML Elemente sind erlaubt: <i>strong, b, em, i, a, span</i>.')),
-        array(
-            'icon' => 'icons/16/black/info.png',
-            'text' => _('Links erhalten automatisch die Attribute <i>target="_blank"</i> und <i>rel="nofollow"</i>.')),
-        array(
-            'text' => _('<small>powered by <a href="https://github.com/xing/wysihtml5" target="_blank">wysihtml5</a></small>'))));
-
-$infobox = array(
-    'picture' => 'infobox/board1.jpg',
-    'content' => $infobox_content);
+include 'infobox.php';
 
 // show the current text from the database
 ?>
