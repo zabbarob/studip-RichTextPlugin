ef3fecec9a1583fd29978df1b7c343617eaf386b
Robert Costa <zabbarob@gmail.com>
2013-06-10 14:14:19 +0200

    forgot to add errors.php in commit b87747ac954d68e7c2d7ab5074fd61cc488a5051

---
+++ templates/errors.php
@@ -0,0 +1,18 @@
+<?
+/**
+ * errors.php - Template for displaying error messages.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * @author      Robert Costa <zabbarob@gmail.com>
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+ * @category    Stud.IP
+ */
+if ($GLOBALS['msg']) {
+    echo MessageBox::error('Warning', $GLOBALS['msg']);
+    $GLOBALS['msg'] = null;
+}
+
