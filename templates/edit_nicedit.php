7cb0d2943454a0c88fc59c88b078b041228aaebd
Robert Costa <zabbarob@gmail.com>
2013-07-10 19:02:40 +0200

    nicedit toolbar is now floating; fixes #26

--- templates/edit_nicedit.php
+++ templates/edit_nicedit.php
@@ -12,8 +12,15 @@
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
  * @category    Stud.IP
  */
+?>
+
+<div id="nicedit-toolbar" style="padding-left:10px; width:98%; z-index:100000;"></div>
+
+<div id="editor-position"> </div>
+<?
 include 'common_edit.php';
 ?>
+
 <script type="text/javascript">
 //MathJax.Hub.config.skipStartupTypeset = true;
 bkLib.onDomLoaded(function(){
@@ -29,9 +36,35 @@ bkLib.onDomLoaded(function(){
         fullPanel: true,
         iconsPath: richTextPlugin.dir + 'nicEditorIcons.gif' // set in script.js
     });
-    nic.panelInstance('richtext-editor');
+    nic.setPanel('nicedit-toolbar');
+    nic.addInstance('richtext-editor');
 
     var editor = nic.instanceById('richtext-editor');
+
+    // resize NicEdit toolbar and editor window
+    var nic_main = $('.nicEdit-main');
+    var nic_panel = $('.nicEdit-panelContain');
+    $(window).on('resize', function(){
+        nic_main.parent().width('100%');
+        nic_main.width('100%');
+        nic_panel.parent().width(nic_main.parent().width());
+        nic_panel.width(nic_main.parent().width());
+    });
+
+    // update the toolbar position to always keep the toolbar on screen
+    var toolbar = $('#nicedit-toolbar');
+    var editor_position = $('#editor-position');// this is a hack since getting position of #richtext-editor returns values relative to the iframe
+    var jq_window = $(window);
+    var updateToolbarPosition = function(){
+        var editor_top = editor_position.offset().top;
+        var window_top = jq_window.scrollTop();
+        toolbar.offset({
+            top: Math.max(0, editor_top - window_top - toolbar.height()) + window_top
+        });
+    };
+    updateToolbarPosition();
+    jq_window.scroll(updateToolbarPosition);
+    jq_window.on('resize', updateToolbarPosition);
 
     // helpers
     var insertHtml = function(html) {
