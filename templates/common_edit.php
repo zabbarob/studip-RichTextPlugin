49f7c750e851cb5a1da7dad61c95eaa255f66ef8
Robert Costa <zabbarob@gmail.com>
2013-10-14 18:46:21 +0200

    writing returned file name of uploaded file to console.log

--- templates/common_edit.php
+++ templates/common_edit.php
@@ -26,12 +26,21 @@ include 'errors.php'; // show errors
                 $('#fileupload').fileupload({
                     dataType: 'json',
                     done: function(e, data) {
-                        console.log('done')
-                        $.each(data.result.files, function(index, file) {
-                            console.log('each file');
-                            console.log(file.name);
-                            //$('<p/>').text(file.name).appendTo(document.body);
-                        });
+                        console.log('done');
+                        // $.each(data.result.files, function(index, file) {
+                        json = data.result;
+                        if (typeof json.inserts === 'object') {
+                            $.each(json.inserts, function(index, file) {
+                                // NOTE StudIP sends SVGs as application/octet-stream
+                                console.log(file);
+                            });
+                        }
+                        if (typeof json.errors === 'object') {
+                            var message = "Es konnten nicht alle Dateien hochgeladen werden.\n\n"
+                            alert(message + json.errors.join('\n'));
+                        } else if (typeof json.inserts !== 'object') {
+                            alert('Das Hochladen der Datei(en) ist fehlgeschlagen.');
+                        }
                     }
                 });
 
