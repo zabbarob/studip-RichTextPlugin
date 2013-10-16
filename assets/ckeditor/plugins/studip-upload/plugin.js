71ca225b07fc477c19a8e2e734432eaf97f8b53b
Robert Costa <zabbarob@gmail.com>
2013-10-16 16:47:02 +0200

    upload previously worked only the first time, now handling seems to be correct :-)

--- assets/ckeditor/plugins/studip-upload/plugin.js
+++ assets/ckeditor/plugins/studip-upload/plugin.js
@@ -1,39 +1,43 @@
 CKEDITOR.plugins.add('studip-upload', {
     icons: 'upload',
     init: function (editor) {
-        var input = $('<input type="file" name="files" data-url="'
-                    + $('#post_files_url').val() + '" multiple />')
-                .css('display', 'none')
-                .appendTo(document.body);
-
-        input.fileupload({
-            dataType: 'json',
-            done: function(e, data) {
-                json = data.result;
-                if (typeof json.inserts === 'object') {
-                    $.each(json.inserts, function(index, file) {
-                        // NOTE StudIP sends SVGs as application/octet-stream
-                        console.log(file);
-                    });
-                }
-                if (typeof json.errors === 'object') {
-                    var message = 'Es konnten nicht alle Dateien'
-                        + ' hochgeladen werden.\n\n';
-                    alert(message + json.errors.join('\n'));
-                } else if (typeof json.inserts !== 'object') {
-                    alert('Das Hochladen der Datei(en) ist fehlgeschlagen.');
-                }
-
-                /*$.each(data.result.files, function(index, file) {
-                    console.log('each file');
-                    console.log(file.name);
-                    //$('<p/>').text(file.name).appendTo(document.body);
-                });*/
-            }
-        });
 
         editor.addCommand('upload', { // command for uploading files
             exec: function(editor) {
+                var input = $('<input type="file" name="files" data-url="'
+                            + $('#post_files_url').val() + '" multiple />')
+                        .css('display', 'none')
+                        .appendTo(document.body);
+
+                input.fileupload({
+                    dataType: 'json',
+                    done: function(e, data) {
+                        console.log('input done');
+                        json = data.result;
+                        if (typeof json.inserts === 'object') {
+                            $.each(json.inserts, function(index, file) {
+                                // NOTE StudIP sends SVGs as application/octet-stream
+                                console.log(file);
+                            });
+                        }
+                        if (typeof json.errors === 'object') {
+                            var message = 'Es konnten nicht alle Dateien'
+                                + ' hochgeladen werden.\n\n';
+                            alert(message + json.errors.join('\n'));
+                        } else if (typeof json.inserts !== 'object') {
+                            alert('Das Hochladen der Datei(en) ist fehlgeschlagen.');
+                        }
+
+                        /*$.each(data.result.files, function(index, file) {
+                            console.log('each file');
+                            console.log(file.name);
+                            //$('<p/>').text(file.name).appendTo(document.body);
+                        });*/
+                        console.log('input.remove()');
+                        input.remove();
+                    }
+                });                
+                console.log('input.click()');
                 input.click();
             }
         });
