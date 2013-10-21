638fefd27fdb0a880b70e83b0359e1e39c7c380d
Robert Costa <zabbarob@gmail.com>
2013-10-21 15:53:06 +0200

    studip-upload plugin now correctly inserts files into document

--- assets/ckeditor/plugins/studip-upload/plugin.js
+++ assets/ckeditor/plugins/studip-upload/plugin.js
@@ -1,27 +1,58 @@
 CKEDITOR.plugins.add('studip-upload', {
     icons: 'upload',
-    init: function (editor) {
-        var handleUploads = function(fileList){
-            if (!fileList) {
-                alert('Das Hochladen der Datei(en) ist fehlgeschlagen.');
-                return;
-            }
-
-            var errors = [];
-            $.each(fileList, function(index, file){
-                console.log(file);
-                if (file.error) {
-                    errors.push(file.name + ': ' + file.error);
+    init: function(editor){
+        var isImage = function(mime_type){
+                return (typeof mime_type) === 'string' && mime_type.match('^image');
+            },
+            isSVG = function(mime_type){
+                return (typeof mime_type) === 'string' && mime_type === 'image/svg+xml';
+            },
+            insertNode = function($node){
+                editor.insertHtml($('<div>').append($node).html());
+            },
+            insertImage = function(file){
+                insertNode($('<img />').attr({
+                    src: file.url,
+                    alt: file.name,
+                    title: file.name
+                }));
+            },
+            insertLink = function(file){
+                insertNode($('<a>').attr({
+                    href: file.url,
+                    type: file.type,
+                    target: '_blank',
+                    rel: 'nofollow'
+                }).append(file.name));
+            },
+            insertFile = function(file){
+                // NOTE StudIP sends SVGs as application/octet-stream
+                if (isImage(file.type) && !isSVG(file.type)) {
+                    insertImage(file);
                 } else {
-                    // NOTE StudIP sends SVGs as application/octet-stream
+                    insertLink(file);
                 }
-            });
-            if (errors.length) {
-                var message = 'Es konnten nicht alle Dateien'
-                    + ' hochgeladen werden.\n\n';
-                alert(message + errors.join('\n'));
-            }
-        }
+            },
+            handleUploads = function(fileList){
+                if (!fileList) {
+                    alert('Das Hochladen der Datei(en) ist fehlgeschlagen.');
+                    return;
+                }
+
+                var errors = [];
+                $.each(fileList, function(index, file){
+                    if (file.error) {
+                        errors.push(file.name + ': ' + file.error);
+                    } else {
+                        insertFile(file);
+                    }
+                });
+                if (errors.length) {
+                    var message = 'Es konnten nicht alle Dateien'
+                        + ' hochgeladen werden.\n\n';
+                    alert(message + errors.join('\n'));
+                }
+            };
 
         editor.addCommand('upload', { // command for uploading files
             exec: function(editor) {
